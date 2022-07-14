daf6bfdec818005460a8d8f4b973f4913b3ea71c
Pheaktra19 <kongsopheaktra96@gmail.com>
2022-07-14 10:02:44 +0700

    Add to git

--- app.js
+++ app.js
@@ -1,54 +1,22 @@
-// new Vue({
-//   el: '#vue-app',
-//   data : {
-//     name: 'Pheaktra',
-//     job: 'Web Developer',
-//     age: ' 20',
-//     website: 'https://www.booking.com/',
-//     x: 0,
-//     y: 0
-//   },
-//   methods: {
-//     greet: function(time){
-//       return 'Good ' + time + ' ' + this.name;
-//     },
-//     Add and Sunstract Number of Age
-//     add: function(inc){ //inc = increase
-//       this.age += inc;
-//     },
-//     Substract: function(dec){ // dec = decrease
-//       this.age -= dec;
-//     },
-//     // Movemouse
-//     updateXY: function(event){
-//       this.x = event.offsetX;
-//       this.y = event.offsetY;
-//     },
-//     //Keybard Event
-//     logName:function() {
-//       console.log('you entered your name.');
-//     },
-//     logAge:function() {
-//       console.log('you entered your age.');
-//     }
-//   }
-// });
-
 new Vue({
-  el: '#vue-app',
-  data:{
-    characters: ['Pheaktra', 'Phanath', 'Bank', 'Man'],
-    admins: [
-      { name: 'Pheaktra', age: 20},
-      { name: 'Phanath', age: 21},
-      { name: 'Bank', age: 22},
-      { name: 'Man', age: 23}
-    ]
+el:'#vue-app',
+data:{
+  health: 100,
+  ended: false
+},
+methods: {
+  punch: function(){
+    this.health -= 10;
+    if(this.health <= 0){
+      this.ended = true;
+    }
   },
-  methods: {
-
-  },
-  computed: {
-
+  restart: function(){
+    this.health = 100;
+      this.ended = false;
   }
+},
+compted:{
+
+}
 });
